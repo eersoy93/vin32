ffd8027c7c593468a6afcb008f9ff4572e94e19b
Erdem Ersoy <erdemersoy256@gmail.com>
2022-10-17 12:05:00 +0300

    Add MZ signature checking for exe
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- vin32.v
+++ vin32.v
@@ -16,5 +16,18 @@ fn main()
 	else
 	{
 		println("Running Vin32...")  // Under construction!!!
+		exe_path := os.args[0]
+		exe_contents := os.read_file(exe_path) or {
+			return
+		}
+
+		if exe_contents[0..2] == "MZ"
+		{
+			println("MZ signature found!!!")
+		}
+		else
+		{
+			println("MZ signature not found!!!")
+		}
 	}
 }
