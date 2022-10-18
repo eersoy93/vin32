f02b958968b0b608e167e2134cb02d6fb781bb85
Erdem Ersoy <erdemersoy256@gmail.com>
2022-10-18 10:58:07 +0300

    Remove some debug code
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- parser.v
+++ parser.v
@@ -14,11 +14,8 @@ fn parse_exe(exe_contents string)
 		vin32_exit(1)
 	}
 
-	// Debugly print pointer to PE header
-	pointer_to_pe_header_bytes := exe_contents[60..64].bytes()  // 0x3C == 60
-	println_debug("Pointer to PE header in bytes: ${pointer_to_pe_header_bytes}")
-
 	// Check PE signature
+	pointer_to_pe_header_bytes := exe_contents[60..64].bytes()  // 0x3C == 60
 	pe_signature_offset := pointer_to_pe_header_bytes[0]
 	pe_signature := exe_contents[(pe_signature_offset)..(pe_signature_offset+4)]
 	if pe_signature.bytes() == [u8(0x50), 0x45, 0x00, 0x00]
