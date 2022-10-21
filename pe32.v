33c983b1455e62a7c503bffceb15aed22dbc46bd
Erdem Ersoy <erdemersoy256@gmail.com>
2022-10-21 07:37:31 +0300

    Arrange pe32.v
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- pe32.v
+++ pe32.v
@@ -2,100 +2,109 @@ module main
 
 // PE32 structs and other data structures
 
-const magic_number = "MZ"
+const
+(
+	magic_number = [u8(0x4D), 0x5A]                   // "MZ"
+	pe32_nt_signature = [u8(0x50), 0x45, 0x00, 0x00]  // "PE\0\0"
+)
 
-struct PE32DOSHeader
+struct PE32
 {
-	magic_number                     u16
-	last_page_of_file_bytes          u16
-	pages_in_file                    u16
-	relocations                      u16
-	size_of_header_in_paragraphs     u16
-	extra_paragraphs_needed_min      u16
-	extra_paragraphs_needed_max      u16
-	initial_sp_value                 u16
-	checksum                         u16
-	initial_ip_value                 u16
-	initial_relative_cs_value        u16
-	relocation_table_file_address    u16
-	overlay_number                   u16
-	reserved_words                [4]u16
-	oem_identifier                   u16
-	oem_information                  u16
-	reserved_words_2             [10]u16
-	pointer_to_pe_header             u16
+	pe32_dos_header struct
+	{
+		magic_number                     u16
+		last_page_of_file_bytes          u16
+		pages_in_file                    u16
+		relocations                      u16
+		size_of_header_in_paragraphs     u16
+		extra_paragraphs_needed_min      u16
+		extra_paragraphs_needed_max      u16
+		initial_sp_value                 u16
+		checksum                         u16
+		initial_ip_value                 u16
+		initial_relative_cs_value        u16
+		relocation_table_file_address    u16
+		overlay_number                   u16
+		reserved_words                [4]u16
+		oem_identifier                   u16
+		oem_information                  u16
+		reserved_words_2             [10]u16
+		pointer_to_pe_header             u16
+	}
+	pe32_nt_signature [4]u8
+	pe32_file_header struct
+	{
+		machine_type         u16
+		sections_count       u16
+		time_date_stamp      u32
+		symbol_table_pointer u32
+		number_of_symbols    u32
+		optional_header_size u16
+		characteristics      u16
+	}
+	pe32_optional_header struct
+	{
+		magic                      u16
+		linker_version_major       u8
+		linker_version_minor       u8
+		code_size                  u32
+		initialized_data_size      u32
+		uninitialized_data_size    u32
+		entry_point                u32
+		base_of_code               u32
+		base_of_data               u32
+		image_size                 u32
+		section_alignment          u32
+		file_alignment             u32
+		os_version_major           u16
+		os_version_minor           u16
+		subsystem_version_major    u16
+		subsystem_version_minor    u16
+		win32_version_value        u16
+		size_of_image              u32
+		size_of_headers            u32
+		checksum                   u32
+		subsystem                  u32
+		dll_characteristics        u16
+		size_of_stack_reserve      u32
+		size_of_stack_commit       u32
+		size_of_heap_reserve       u32
+		size_of_heap_commit        u32
+		loader_flags               u32
+		rvas_and_sizes_number      u32
+		export_table               u32
+		import_table               u32
+		resource_table             u32
+		exception_table            u32
+		certificate_table          u32
+		base_relocation_table      u32
+		debug_table                u32
+		architecture_specific_data u32
+		rva_of_global_pointer      u32
+		tls_table                  u32
+		load_config_table          u32
+		bound_import_table         u32
+		import_address_table       u32
+		delay_import_descriptor    u32
+		clr_runtime_header         u32
+	}
+	section_header struct
+	{
+		name                   u64
+		virtual_size           u32
+		virtual_address        u32
+		sizeof_raw_data        u32
+		ptr_to_raw_data        u32
+		ptr_to_relocations     u32
+		ptr_to_line_numbers    u32
+		number_of_relocations  u16
+		number_of_line_numbers u16
+		characteristics        u32
+	}
 }
 
-const pe32_nt_signature = [u8(0x50), 0x45, 0x00, 0x00]  // "PE\0\0"
 
-struct PE32FileHeader
-{
-	machine_type         u16
-	sections_count       u16
-	time_date_stamp      u32
-	symbol_table_pointer u32
-	number_of_symbols    u32
-	optional_header_size u16
-	characteristics      u16
-}
 
-struct PE32OptionalHeader
-{
-	magic                      u16
-	linker_version_major       u8
-	linker_version_minor       u8
-	code_size                  u32
-	initialized_data_size      u32
-	uninitialized_data_size    u32
-	entry_point                u32
-	base_of_code               u32
-	base_of_data               u32
-	image_size                 u32
-	section_alignment          u32
-	file_alignment             u32
-	os_version_major           u16
-	os_version_minor           u16
-	subsystem_version_major    u16
-	subsystem_version_minor    u16
-	win32_version_value        u16
-	size_of_image              u32
-	size_of_headers            u32
-	checksum                   u32
-	subsystem                  u32
-	dll_characteristics        u16
-	size_of_stack_reserve      u32
-	size_of_stack_commit       u32
-	size_of_heap_reserve       u32
-	size_of_heap_commit        u32
-	loader_flags               u32
-	rvas_and_sizes_number      u32
-	export_table               u32
-	import_table               u32
-	resource_table             u32
-	exception_table            u32
-	certificate_table          u32
-	base_relocation_table      u32
-	debug_table                u32
-	architecture_specific_data u32
-	rva_of_global_pointer      u32
-	tls_table                  u32
-	load_config_table          u32
-	bound_import_table         u32
-	import_address_table       u32
-	delay_import_descriptor    u32
-	clr_runtime_header         u32
-}
 
-struct SectionHeader
-{
-	name                   u64
-	virtual_size           u32
-	virtual_address        u32
-	sizeof_raw_data        u32
-	ptr_to_raw_data        u32
-	ptr_to_relocations     u32
-	ptr_to_line_numbers    u32
-	number_of_relocations  u16
-	number_of_line_numbers u16
-	characteristics        u32
-}
+
+
