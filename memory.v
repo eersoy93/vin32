55cedb28ced8e5b5d1609968328ac0348b2ce328
Erdem Ersoy <erdemersoy256@gmail.com>
2022-10-27 09:02:25 +0300

    Some changes:
    - Correct SizeOfHeaders definition of the EXE
    - Define memory -> Initialize memory
    
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- memory.v
+++ memory.v
@@ -1,6 +1,6 @@
 module main
 
-// Define (initialize) EXE memory
+// Initialize (define) EXE memory
 fn define_memory(image_size u32) []u8
 {
 	return []u8{len: int(image_size), cap: int(image_size), init: 0}
