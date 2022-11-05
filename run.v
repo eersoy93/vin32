4c038eff476d07c9bc5b0bc5fff9b3d982544066
Erdem Ersoy <erdemersoy256@gmail.com>
2022-11-05 12:19:38 +0300

    init_memory() -> init_exe_memory()
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- run.v
+++ run.v
@@ -9,7 +9,7 @@ fn run_exe(exe_contents string) int
 	check_exe(pe32_dos_header, pe32_file_header, pe32_optional_header, pe32_section_headers)
 
 	// Initialize EXE memory
-	mut memory := init_memory(pe32_optional_header.size_of_image)
+	mut memory := init_exe_memory(pe32_optional_header.size_of_image)
 	if memory.len == pe32_optional_header.size_of_image
 	{
 		println_debug("The EXE memory has been initialized with EXE image size bytes.")
