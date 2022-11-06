6ce913d0e0b4ea659981ca1ba5aa5aa80be56725
Erdem Ersoy <erdemersoy256@gmail.com>
2022-11-06 08:27:48 +0300

    Some changes:
    - Parse import descriptors of the EXE
    - memory -> exe_memory
    
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- run.v
+++ run.v
@@ -8,46 +8,61 @@ fn run_exe(exe_contents string) int
 	// Check some classified EXE content
 	check_exe(pe32_dos_header, pe32_file_header, pe32_optional_header, pe32_section_headers)
 
-	// Initialize EXE memory
-	mut memory := init_exe_memory(pe32_optional_header.size_of_image)
-	if memory.len == pe32_optional_header.size_of_image
+	// Initialize EXE exe_memory
+	mut exe_memory := init_exe_memory(pe32_optional_header.size_of_image)
+	if exe_memory.len == pe32_optional_header.size_of_image
 	{
-		println_debug("The EXE memory has been initialized with EXE image size bytes.")
+		println_debug("The EXE exe_memory has been initialized with EXE image size bytes.")
 	}
 	else
 	{
-		println_error("The EXE memory has not been initialized with EXE image size bytes.")
+		println_error("The EXE exe_memory has not been initialized with EXE image size bytes.")
 	}
 
-	// Load headers to EXE memory
+	// Load headers to EXE exe_memory
 
 	for i in 0..64  // For EXE DOS Header
 	{
-		memory[i] = exe_contents[i]
+		exe_memory[i] = exe_contents[i]
 	}
 
 	for i in pe_header_pointer..(pe_header_pointer + 248)  // For EXE File Header AND EXE Optional Header
 	{
-		memory[i] = exe_contents[i]
+		exe_memory[i] = exe_contents[i]
 	}
 
 	for i in 0..(exe_sections_count)  // For EXE section headers
 	{
 		for j in 248..288
 		{
-			memory[pe_header_pointer + 40 * i + j] = exe_contents[pe_header_pointer + 40 * i + j]
+			exe_memory[pe_header_pointer + 40 * i + j] = exe_contents[pe_header_pointer + 40 * i + j]
 		}
 	}
 
-	// Load sections to EXE memory
+	// Load sections to EXE exe_memory
 	for section in pe32_section_headers
 	{
 		for j in 0..(section.sizeof_raw_data)
 		{
-			memory[section.virtual_address + j] = exe_contents[section.ptr_to_raw_data + j]
+			exe_memory[section.virtual_address + j] = exe_contents[section.ptr_to_raw_data + j]
 		}
 		name := section.name[..]
 		println_debug("${name.bytestr()} section has been loaded.")
+	}
+
+	// Parse imports on the EXE exe_memory
+	import_directories_address := pe32_optional_header.import_table.address
+	import_directories_size := pe32_optional_header.import_table.size
+	import_directories_count := import_directories_size / sizeof(PE32_IMPORT_DESCRIPTOR)
+	pe32_import_descriptors := []PE32_IMPORT_DESCRIPTOR
+	{ len: int(import_directories_count), cap: int(import_directories_count), init: PE32_IMPORT_DESCRIPTOR
+		{
+			original_first_thunk: u32(exe_memory[(import_directories_address + u32(it) * import_directories_size)..(import_directories_address + u32(it) * import_directories_size + 4)].reverse().hex().parse_uint(16, 0) or { panic })
+			time_date_stamp:      u32(exe_memory[(import_directories_address + u32(it) * import_directories_size + 4)..(import_directories_address + u32(it) * import_directories_size + 8)].reverse().hex().parse_uint(16, 0) or { panic })
+			forwarder:            u32(exe_memory[(import_directories_address + u32(it) * import_directories_size + 8)..(import_directories_address + u32(it) * import_directories_size + 12)].reverse().hex().parse_uint(16, 0) or { panic })
+			rva_of_name:          u32(exe_memory[(import_directories_address + u32(it) * import_directories_size + 12)..(import_directories_address + u32(it) * import_directories_size + 16)].reverse().hex().parse_uint(16, 0) or { panic })
+			first_thunk:          u32(exe_memory[(import_directories_address + u32(it) * import_directories_size + 16)..(import_directories_address + u32(it) * import_directories_size + 20)].reverse().hex().parse_uint(16, 0) or { panic })
+		}
 	}
 
 	// TODO: To be continued!
