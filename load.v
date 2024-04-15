b69a2213134724107835651f87fb519523afcb91
Erdem Ersoy <erdemersoy256@gmail.com>
2024-04-15 18:21:26 +0300

    Add some debug prints

--- load.v
+++ load.v
@@ -3,17 +3,19 @@ module main
 fn load_exe(exe_contents string, mut exe_memory []u8, pe32_dos_header PE32_DOS_HEADER, pe32_file_header PE32_FILE_HEADER, pe32_optional_header PE32_OPTIONAL_HEADER, pe32_section_headers []PE32_SECTION_HEADER, pe_header_pointer u32, exe_sections_count int)
 {
 	// Load headers to EXE exe_memory
-
+	println_debug("Loading DOS header to EXE memory...")
 	for i in 0..64  // For EXE DOS Header
 	{
 		exe_memory[i] = exe_contents[i]
 	}
 
+	println_debug("Loading PE header to EXE memory...")
 	for i in pe_header_pointer..(pe_header_pointer + 248)  // For EXE File Header AND EXE Optional Header
 	{
 		exe_memory[i] = exe_contents[i]
 	}
 
+	println_debug("Loading section headers to EXE memory...")
 	for i in 0..(exe_sections_count)  // For EXE section headers
 	{
 		for j in 248..288
@@ -23,6 +25,7 @@ fn load_exe(exe_contents string, mut exe_memory []u8, pe32_dos_header PE32_DOS_H
 	}
 
 	// Load sections to EXE exe_memory
+	println_debug("Loading sections to EXE memory...")
 	for section in pe32_section_headers
 	{
 		for j in 0..(section.sizeof_raw_data)
